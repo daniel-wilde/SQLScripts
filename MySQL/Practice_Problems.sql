ee985fd5f76660b446b71360bb8f9bb6c206e660
danlo924 <daniel.r.wilde@gmail.com>
2022-04-24 09:41:47 -0700

    updated

--- MySQL/Practice_Problems.sql
+++ MySQL/Practice_Problems.sql
@@ -58,16 +58,114 @@ WHERE EXISTS
         AND m.salary < e.salary
 )
 
+-- Departmet Highest Salary - Mine
+WITH emp_sals AS
+(
+    SELECT 
+        d.name as Department
+        ,e.name as Employee
+        ,e.Salary 
+        ,RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) as rnk
+    FROM Department d
+        INNER JOIN Employee e on d.id = e.departmentid
+)
+SELECT 
+    e.Department
+    ,e.Employee
+    ,e.Salary
+FROM emp_sals e
+where e.rnk = 1
 
+-- Departmet Highest Salary - CTE
+WITH max_sals AS
+(
+    SELECT 
+        d.id as dept_id
+        ,d.name AS Department
+        ,MAX(e.Salary) as Salary
+    FROM Department d
+        INNER JOIN Employee e on d.id = e.departmentid
+    GROUP BY d.id
+)
+SELECT 
+    m.Department
+    ,e.name as Employee
+    ,e.Salary
+FROM max_sals m
+    INNER JOIN Employee e on m.dept_id = e.departmentID
+        AND m.Salary = e.salary
 
+-- Departmet Highest Salary - OPTIMAL:
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+    )
+;
 
+-- Managers with at least 5 employees
+WITH managers_5r AS
+(
+    SELECT managerID
+    FROM Employee
+    GROUP BY managerID
+    HAVING COUNT(*) >= 5
+)
+SELECT name
+FROM Employee e
+    INNER JOIN managers_5r m ON e.id = m.managerID;
 
+-- TOP 3 Salaries in each department
+WITH top3 AS 
+(
+    SELECT
+        id
+        ,DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) as RNK
+    FROM Employee
+)
+SELECT 
+    d.name as Department
+    ,e.name AS Employee
+    ,e.salary
+FROM Employee e 
+    INNER JOIN top3 t ON e.id = t.id AND t.RNK <= 3
+    INNER JOIN Department d on e.departmentId = d.id
 
+-- Customers who never order
+SELECT name AS Customers
+FROM Customers c
+WHERE NOT EXISTS (
+    select 1 
+    from Orders o 
+    where o.customerId = c.id
+    LIMIT 1
+)
 
-
-
-
-
+-- Friend Requests I: Overall Acceptance Rate 
+WITH ac AS 
+(
+    SELECT requester_id, accepter_id 
+    from RequestAccepted 
+    GROUP BY requester_id, accepter_id 
+),
+rq AS
+(
+    SELECT sender_id, send_to_id
+    from FriendRequest  
+    GROUP BY sender_id, send_to_id
+)
+SELECT ROUND( COALESCE( (SELECT COUNT(*) FROM ac) / (SELECT COUNT(*) FROM rq), 0) , 2) as accept_rate 
 
 
 
