187038ce9aece072cec44d9de13419254f2e0716
Dan Wilde <dan.wilde@cybercoders.com>
2023-09-27 16:12:23 -0700

    latest updates

--- SQLServer/IndexFragmentation.sql
+++ SQLServer/IndexFragmentation.sql
@@ -5,6 +5,6 @@ SELECT OBJECT_NAME(ind.OBJECT_ID) AS TableName
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) indexstats 
 INNER JOIN sys.indexes ind ON ind.object_id = indexstats.object_id 
 	AND ind.index_id = indexstats.index_id 
-WHERE Object_name(ind.object_id) = 'TableName'
+WHERE Object_name(ind.object_id) = 'TBL'
 	--AND indexstats.avg_fragmentation_in_percent > 30 
 ORDER BY indexstats.avg_fragmentation_in_percent DESC 
